879d338a512d7440e1c8941cc45a99730924ed39
Ana Martínez Saucedo <anmartinez@uade.edu.ar>
2023-09-08 15:04:51 -0300

    feat: CORS support

--- backend/recommender/app.py
+++ backend/recommender/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from backend.recommender.routes.recommendations import recommendations
 from backend.recommender.routes.models import models
@@ -6,8 +7,9 @@ from backend.recommender.routes.apis import apis
 
 app = Flask(__name__)
 
-app.register_blueprint(recommendations)
-app.register_blueprint(models)
-app.register_blueprint(apis)
+app.register_blueprint(recommendations, url_prefix="/api/v1")
+app.register_blueprint(models, url_prefix="/api/v1")
+app.register_blueprint(apis, url_prefix="/api/v1")
 
+CORS(app)
 
